3f8cccafa12dc4b244d9f341b4215348bf05569d
Vikesh Kumar <kumarvikesh287@gmail.com>
2025-06-17 17:47:11 +0530

    frontend setup---

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-8xl font-bold underline font-extrabold">Hello world!</h1>
+      <Button onClick={() => alert('Shadcn UI Button Clicked!')}>Shadcn Button</Button>
     </div>
   );
 }
